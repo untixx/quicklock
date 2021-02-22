606469d8cf37d6985b8cdcb9bbfe4d7293428e56
Mathias Ziebarth <mathias.ziebarth@iosb.fraunhofer.de>
2021-02-22 13:08:29 +0100

    updated setup.py for new minor release

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
+
 setup(
-  name = 'quicklock',
-  packages = ['quicklock'],
-  version = '0.1.7',
-  description = 'A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
-  author = 'Nate Ferrero',
-  author_email = 'nateferrero@gmail.com',
-  url = 'https://github.com/NateFerrero/quicklock',
-  download_url = 'https://github.com/NateFerrero/quicklock/tarball/0.1.7',
-  keywords = ['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
-  classifiers = [],
-  platforms='any',
-  install_requires = [
-    'psutil>=2.2'
-  ]
+    name='quicklock',
+    packages=['quicklock'],
+    version='0.1.8',
+    description='A simple Python resource lock to ensure only one process at a time is operating with a particular resource.',
+    author='Mathias Ziebarth (forked from Nate Ferrero)',
+    author_email='mathias.ziebarth@gmail.com',
+    url='https://github.com/untixx/quicklock/',
+    download_url='https://github.com/untixx/quicklock/tarball/0.1.8',
+    keywords=['lock', 'locking', 'singleton', 'process', 'resource', 'exclusive lock'],
+    classifiers=[],
+    platforms='any',
+    install_requires=[
+        'psutil>=2.2'
+    ]
 )
